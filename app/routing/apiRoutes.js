c3b1aabda98849b0a2a033a2a22a662c962872dd
AgesilausDrako <agesilaus@yahoo.com>
2017-08-20 20:17:44 -0400

    added routing files

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,30 @@
+ // Search for Specific Character (or all characters) - provides JSON
+ app.get("/api/:characters?", function(req, res) {
+    var chosen = req.params.characters;
+  
+    if (chosen) {
+      console.log(chosen);
+  
+      for (var i = 0; i < characters.length; i++) {
+        if (chosen === characters[i].routeName) {
+          return res.json(characters[i]);
+        }
+      }
+  
+      return res.json(false);
+    }
+    return res.json(characters);
+  });
+  
+  // Create New Characters - takes in JSON input
+  app.post("/api/new", function(req, res) {
+    var newcharacter = req.body;
+    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newcharacter);
+  
+    characters.push(newcharacter);
+  
+    res.json(newcharacter);
+  });
+  
